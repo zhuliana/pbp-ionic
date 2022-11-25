845bf575e7a985f490f85d04c7e1b6e86fbdd7b2
zhuliana <zhuliana@students.undip.ac.id>
2022-11-25 09:18:25 +0700

    add api service gedung

---
+++ src/app/api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    public http: HttpClient,
+    ) {
+
+     }
+  
+ //link API
+  apiURL() {
+    return "http://localhost/api";
+  }
+  
+  getGedung() {
+    return this.http.get(this.apiURL() + '/view_gedung.php');
+  }
+  
+  deleteGedung(id: string) {
+    return this.http.delete(this.apiURL() + '/delete_gedung.php?idGedung=' + id);
+  }
+  
+  viewGedung(id: string) {
+    return this.http.get(this.apiURL() + '/viewGedung.php?idGedung=' + id);
+  }
+
+}
+
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'loader',
+    redirectTo: 'gedung',
     pathMatch: 'full'
   },
   {
     path: 'loader',
     loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)
-  },  {
+  },
+  {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
